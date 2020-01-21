659513d9005f038755219f2cc386b3d84bbd1127
Ryan Ziegenfus <thewebcoderz@gmail.com>
2020-01-20 21:59:45 -0500

    finished bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,27 +4,15 @@ def selection_sort( arr ):
     for i in range(0, len(arr) - 1):
         smallest_index = i
         smallest_value = arr[i]
-        print(f'This index at i is: {i}')
-        print(f'This value at i is: {arr[i]}')
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         for j in range(i + 1, len(arr)):
-            print(f'The index at j is: {j}')
-            print(f'The value at j is: {arr[j]}')
             if arr[j] < smallest_value:
-                print(f'{arr[j]} is less than {smallest_value}')
                 smallest_value = arr[j]
                 smallest_index = j
-                print(f'The new smallest value is: {smallest_value}')
-
-            print('************')
         
         if smallest_value != arr[i]:
-            print(f'{arr[i]} becomes {smallest_value}')
-            print(f'{arr[smallest_index]} becomes {arr[i]}')
-
             arr[i], arr[smallest_index] = smallest_value, arr[i]
-        print(f'This is the updated array: {arr}')   
         # TO-DO: swap
 
 
@@ -35,7 +23,13 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for _ in range(0, len(arr) - 1):
+        j = len(arr) - 1
+        for k in range(0, j):
+            if arr[k] > arr[k + 1]:
+                arr[k], arr[k + 1] = arr[k + 1], arr[k]
+        j = j - 1
+    print(arr)
     return arr
 
 
@@ -45,4 +39,4 @@ def count_sort( arr, maximum=-1 ):
     return arr
 
 
-selection_sort([3,5,1,2,3])
+bubble_sort([3, 5, 1])
